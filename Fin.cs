c3a9c5dbc0c649cf272fd51cffed7cbe9835a8bf
Daniil <rychenkov16@yandex.ru>
2024-09-21 22:13:04 +0300

    Добавьте файлы проекта.

---
+++ Fin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcInterface
+{
+    public class Fin1 : IFin
+    {
+        ILogger Logger { get; }
+        public Fin1 (ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Fin()
+        {
+            Logger.Event("Суммирование завершено");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace CalcInterface
+{
+
+    class Program
+    {
+
+        static ILogger Logger { get; set; }
+        static void Main()
+        {
+            Logger = new Logger();
+            Console.WriteLine("Введите 2 целочисленных значения: ");
+            try
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                int b = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Неверно введены значения");
+            }
+            var fin1 = new Fin1(Logger);
+            MiniCalc minicalc = new MiniCalc();
+            minicalc.Sum(a, b);
+
+            fin1.Fin();
+            Console.ReadKey();
+
+        }
+        
+
+    }
+
+    public interface IMiniCalc
+    {
+        void Sum(int a, int b);
+    }
+
+    public class MiniCalc : IMiniCalc
+    {
+        public void Sum(int a, int b)
+        {
+            Console.WriteLine("Сумма введенных значений: {0}", a + b);
+        }
+    }
+
+    public interface ILogger
+    {
+        void Event(string message);
+    }
+
+    public class Logger : ILogger
+    {
+        public void Event(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine(message);
+        }
+    }
+
+    public interface IFin
+    {
+        void Fin();
+    }
+}
